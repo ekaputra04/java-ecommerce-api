4f35b129935e3393b127f8d698e61a542b584ab4
ekaputra04 <ekaputrajuniawan@gmail.com>
2023-05-17 18:09:40 +0800

    Membuat Class MyHandler yaitu kelas implementasi HttpHandler yang akan menangani permintaan HTTP dari klien

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/api", new MyHandler()); // Menetapkan handler ke path tertentu
+        server.setExecutor(null); // Menggunakan executor default
+        server.start(); // Memulai server
     }
 }
---
+++ src/MyHandler.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+
+public class MyHandler implements HttpHandler{
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = "Hello, World!"; // Contoh respon API
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(response.getBytes());
+        outputStream.close();
+    }
+}
