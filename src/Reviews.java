c72e3d37ae533a39ef91e81f36354f2fe57ae993
ekaputra04 <ekaputrajuniawan@gmail.com>
2023-05-26 17:52:38 +0800

    membuat class Reviews sebagai model entitas

---
+++ src/Reviews.java
@@ -0,0 +1,35 @@
+public class Reviews {
+    private int order;
+    private int star;
+    private String description;
+
+    public Reviews(int order, int star, String description) {
+        this.order = order;
+        this.star = star;
+        this.description = description;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
