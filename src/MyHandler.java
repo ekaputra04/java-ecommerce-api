89f80ac4d45ea7c5e74da73d39bec841e934c8dc
ekaputra04 <ekaputrajuniawan@gmail.com>
2023-05-26 17:21:25 +0800

    menambahkan Sqlite dalam project java dan membuat class Addresses sebagai model entitas

--- src/MyHandler.java
+++ src/MyHandler.java
@@ -10,6 +10,8 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class MyHandler implements HttpHandler{
+    private static final String DB_URL = "jdbc:sqlite:/C/Sqlite/db_ecommerce";
+    
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
