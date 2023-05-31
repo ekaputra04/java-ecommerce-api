540d3295adec2564bbde11c6ebafbd6e64177a37
ekaputra04 <ekaputrajuniawan@gmail.com>
2023-05-31 14:01:25 +0800

    menambahkan response dalam json pada Handler put

--- src/HandlerPutRequest.java
+++ src/HandlerPutRequest.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.sql.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Class `HandlerPutRequest` menangani permintaan HTTP dengan metode PUT.
@@ -16,9 +18,12 @@ public class HandlerPutRequest {
     private static String response;
     private static String tableName;
     private static String[] pathSegments;
+    private static JSONArray data = new JSONArray();
 
     public static void handlePutRequest(HttpExchange exchange) throws SQLException, IOException {
+        data.clear();
         OutputStream outputStream = exchange.getResponseBody();
+        JSONObject jsonObject = new JSONObject();
 
         // Mendapatkan path dari permintaan
         path = exchange.getRequestURI().getPath();
@@ -30,8 +35,9 @@ public class HandlerPutRequest {
             tableName = pathSegments[1];
             id = pathSegments[2];
         } else {
-            response = "Invalid path. Please specify a valid table name.";
-            statusCode = 200;
+            jsonObject.put("status_code", 200);
+            jsonObject.put("message", "Invalid path. Please specify a valid table name. " + tableName + " is unvalid.");
+            data.put(jsonObject);
         }
 
         InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
@@ -51,64 +57,78 @@ public class HandlerPutRequest {
             Users user = new Users();
             if (user.parseUserJSON(json) != 1) {
                 user.updateUser(id);
-                response = "Data has been successfully updated.";
-                statusCode = 200;
+                jsonObject.put("status_code", 200);
+                jsonObject.put("message", "Data " + tableName + " has been successfully updated.");
+                data.put(jsonObject);
             } else {
-                response = "Data is not valid.";
-                statusCode = 400;
+                jsonObject.put("status_code", 400);
+                jsonObject.put("message", "Data in " + tableName + " is not valid.");
+                data.put(jsonObject);
             }
         } else if (tableName.equals("addresses")) {
             Addresses addresses = new Addresses();
             if (addresses.parseAddressesJSON(json) != 1) {
                 addresses.updateAddress(id);
-                response = "Data has been successfully updated.";
-                statusCode = 200;
+                jsonObject.put("status_code", 200);
+                jsonObject.put("message", "Data " + tableName + " has been successfully updated.");
+                data.put(jsonObject);
             } else {
-                response = "Data is not valid.";
-                statusCode = 400;
+                jsonObject.put("status_code", 400);
+                jsonObject.put("message", "Data in " + tableName + " is not valid.");
+                data.put(jsonObject);
             }
         } else if (tableName.equals("products")) {
             Products products = new Products();
             if (products.parseProductsJSON(json) != 1) {
                 products.updateProduct(id);
-                response = "Data has been successfully updated.";
-                statusCode = 200;
+                jsonObject.put("status_code", 200);
+                jsonObject.put("message", "Data " + tableName + " has been successfully updated.");
+                data.put(jsonObject);
             } else {
-                response = "Data is not valid.";
-                statusCode = 400;
+                jsonObject.put("status_code", 400);
+                jsonObject.put("message", "Data in " + tableName + " is not valid.");
+                data.put(jsonObject);
             }
         } else if (tableName.equals("orders")) {
             Orders orders = new Orders();
             if (orders.parseOrdersJSON(json) != 1) {
                 orders.updateOrder(id);
-                response = "Data has been successfully updated.";
-                statusCode = 200;
+                jsonObject.put("status_code", 200);
+                jsonObject.put("message", "Data " + tableName + " has been successfully updated.");
+                data.put(jsonObject);
             } else {
-                response = "Data is not valid.";
-                statusCode = 400;
+                jsonObject.put("status_code", 400);
+                jsonObject.put("message", "Data in " + tableName + " is not valid.");
+                data.put(jsonObject);
             }
         } else if (tableName.equals("order_details")) {
             OrderDetails orderDetails = new OrderDetails();
             if (orderDetails.parseOrderDetailsJSON(json) != 1) {
                 orderDetails.updateOrderDetails(id);
-                response = "Data has been successfully updated.";
-                statusCode = 200;
+                jsonObject.put("status_code", 200);
+                jsonObject.put("message", "Data " + tableName + " has been successfully updated.");
+                data.put(jsonObject);
             } else {
-                response = "Data is not valid.";
-                statusCode = 400;
+                jsonObject.put("status_code", 400);
+                jsonObject.put("message", "Data in " + tableName + " is not valid.");
+                data.put(jsonObject);
             }
         } else if (tableName.equals("reviews")) {
             Reviews reviews = new Reviews();
             if (reviews.parseReviewsJSON(json) != 1) {
                 reviews.updateReview(id);
-                response = "Data has been successfully updated.";
-                statusCode = 200;
+                jsonObject.put("status_code", 200);
+                jsonObject.put("message", "Data " + tableName + " has been successfully updated.");
+                data.put(jsonObject);
             } else {
-                response = "Data is not valid.";
-                statusCode = 400;
+                jsonObject.put("status_code", 400);
+                jsonObject.put("message", "Data in " + tableName + " is not valid.");
+                data.put(jsonObject);
             }
         }
 
+        statusCode = jsonObject.getInt("status_code");
+        response = data.toString(2);
         exchange.sendResponseHeaders(statusCode, response.length());
         outputStream.write(response.getBytes());
         outputStream.flush();
