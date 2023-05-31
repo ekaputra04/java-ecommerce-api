0d5f89269499008b69302eb7e0ee28e1646f3e28
ekaputra04 <ekaputrajuniawan@gmail.com>
2023-05-31 13:47:00 +0800

    menambahkan response dalam json pada Handler get dan post

--- src/HandlerGetRequest.java
+++ src/HandlerGetRequest.java
@@ -1,6 +1,7 @@
 import com.sun.net.httpserver.HttpExchange;
 import java.sql.*;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Class `HandlerGetRequest` menangani permintaan HTTP dengan metode GET.
@@ -11,6 +12,8 @@ public class HandlerGetRequest {
     private static String id;
     private static String path;
     private static String query;
+    private static String response;
+    private static int statusCode;
     private static String[] pathSegments;
     private static JSONArray data = new JSONArray();
     private static ResultSet resultSet;
@@ -18,7 +21,8 @@ public class HandlerGetRequest {
     private static Statement statement;
 
     public static String handleGetRequest(HttpExchange exchange) throws SQLException {
-        try {
+        JSONObject jsonObject = new JSONObject();
+        try {                    
             // Menghapus data pada JSONArray untuk menghindari duplikasi data
             data.clear();
 
@@ -58,13 +62,23 @@ public class HandlerGetRequest {
                 }
             } else {
                 // Jika tidak ada nama tabel, kembalikan respon error
-                return "Invalid path. Please specify a valid table name.";
+                statusCode = 400;
+                jsonObject.put("status_code", 400);
+                jsonObject.put("message",
+                        "Invalid path. Please specify a valid table name. Table " + tableName + " is unavailable");
+                data.put(jsonObject);
+                // return "Invalid path. Please specify a valid table name.";
             }
 
             // Memeriksa apakah tabel valid
             if (!isValidTable(tableName)) {
                 // Jika tabel tidak valid, kembalikan respon error
-                return "Invalid table name.";
+                statusCode = 400;
+                jsonObject.put("status_code", 400);
+                jsonObject.put("message",
+                        "Invalid path. Please specify a valid table name. Table " + tableName + " is unavailable");
+                data.put(jsonObject);
+                // return "Invalid table name.";
             }
 
             resultSet = statement.executeQuery(query);
@@ -72,6 +86,7 @@ public class HandlerGetRequest {
             // Mengambil hasil query dan menyimpannya dalam JSONArray
             while (resultSet.next()) {
                 if (tableName.equals("users")) {
+                    statusCode = 200;
                     Users users = new Users();
                     users.setId(resultSet.getInt("users"));
                     users.setFirstName(resultSet.getString("first_name"));
@@ -81,6 +96,7 @@ public class HandlerGetRequest {
                     users.setType(resultSet.getString("tipe"));
                     data.put(users.toJsonObject());
                 } else if (tableName.equals("addresses")) {
+                    statusCode = 200;
                     Addresses addresses = new Addresses();
                     addresses.setUsers(resultSet.getInt("id"));
                     addresses.setType(resultSet.getString("tipe"));
@@ -91,6 +107,7 @@ public class HandlerGetRequest {
                     addresses.setPostcode(resultSet.getString("postcode"));
                     data.put(addresses.toJsonObject());
                 } else if (tableName.equals("products")) {
+                    statusCode = 200;
                     Products products = new Products();
                     products.setId(resultSet.getInt("id"));
                     products.setSeller(resultSet.getInt("seller"));
@@ -100,6 +117,7 @@ public class HandlerGetRequest {
                     products.setStock(resultSet.getInt("stock"));
                     data.put(products.toJsonObject());
                 } else if (tableName.equals("orders")) {
+                    statusCode = 200;
                     Orders orders = new Orders();
                     orders.setId(resultSet.getInt("id"));
                     orders.setBuyer(resultSet.getInt("buyer"));
@@ -109,6 +127,7 @@ public class HandlerGetRequest {
                     orders.setIsPaid(resultSet.getBoolean("is_paid"));
                     data.put(orders.toJsonObject());
                 } else if (tableName.equals("reviews")) {
+                    statusCode = 200;
                     Reviews reviews = new Reviews();
                     reviews.setReviewId(resultSet.getInt("review_id"));
                     reviews.setStar(resultSet.getInt("star"));
@@ -116,6 +135,7 @@ public class HandlerGetRequest {
                     reviews.setOrderId(resultSet.getInt("order_id"));
                     data.put(reviews.toJsonObject());
                 } else if (tableName.equals("order_details")) {
+                    statusCode = 200;
                     OrderDetails orderDetails = new OrderDetails();
                     orderDetails.setOrderId(resultSet.getInt("order_id"));
                     orderDetails.setProduct(resultSet.getInt("product"));
@@ -134,8 +154,8 @@ public class HandlerGetRequest {
         }
 
         // Mengatur indentasi pada JSON
-        String response = data.toString(2);
-        return response;
+        response = data.toString(2);
+        return response;        
     }
 
     private static boolean isValidTable(String tableName) {
--- src/HandlerPostRequest.java
+++ src/HandlerPostRequest.java
@@ -2,8 +2,10 @@ import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.sql.*;
+import java.sql.SQLException;
 import java.io.OutputStream;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Class `HandlerPostRequest` menangani permintaan HTTP dengan metode POST.
@@ -14,103 +16,124 @@ public class HandlerPostRequest {
     private static String tableName;
     private static String[] pathSegments;
     private static int statusCode;
-    private static String response;
+    private static JSONArray data = new JSONArray();
 
     public static void handlePostRequest(HttpExchange exchange) throws SQLException, IOException {
-
+        data.clear();
         OutputStream outputStream = exchange.getResponseBody();
+        JSONObject jsonObject = new JSONObject();
 
-        // Mendapatkan path dari permintaan
-        path = exchange.getRequestURI().getPath();
+        try {
+            // Mendapatkan path dari permintaan
+            path = exchange.getRequestURI().getPath();
 
-        // Memisahkan path menjadi endpoint dan id
-        pathSegments = path.split("/");
+            // Memisahkan path menjadi endpoint dan id
+            pathSegments = path.split("/");
 
-        if (pathSegments.length == 2) {
-            tableName = pathSegments[1];
-        } else {
-            response = "Invalid path. Please specify a valid table name.";
-            statusCode = 200;
+            if (pathSegments.length == 2) {
+                tableName = pathSegments[1];
+            } else {
+                jsonObject.put("status_code", 400);
+                jsonObject.put("message",
+                        "Invalid path. Please specify a valid path. Path " + path + " is unavailable for POST method.");
+                data.put(jsonObject);
+            }
+
+            InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            int i;
+            StringBuilder buf = new StringBuilder();
+
+            while ((i = br.read()) != -1) {
+                buf.append((char) i);
+            }
+
+            br.close();
+            isr.close();
+            String json = buf.toString();
+
+            if (tableName.equals("users")) {
+                Users user = new Users();
+                if (user.parseUserJSON(json) != 1) {
+                    user.insertUser();
+                    jsonObject.put("status_code", 200);
+                    jsonObject.put("message", "Data " + tableName + " has been successfully inserted.");
+                    data.put(jsonObject);
+                } else {
+                    jsonObject.put("status_code", 400);
+                    jsonObject.put("message", "Invalid data. Please insert a " + tableName + " valid data.");
+                    data.put(jsonObject);
+                }
+            } else if (tableName.equals("addresses")) {
+                Addresses addresses = new Addresses();
+                if (addresses.parseAddressesJSON(json) != 1) {
+                    addresses.insertAddress();
+                    jsonObject.put("status_code", 200);
+                    jsonObject.put("message", "Data " + tableName + " has been successfully inserted.");
+                    data.put(jsonObject);
+                } else {
+                    jsonObject.put("status_code", 400);
+                    jsonObject.put("message", "Invalid data. Please insert a " + tableName + " valid data.");
+                    data.put(jsonObject);
+                }
+            } else if (tableName.equals("products")) {
+                Products products = new Products();
+                if (products.parseProductsJSON(json) != 1) {
+                    products.insertProduct();
+                    jsonObject.put("status_code", 200);
+                    jsonObject.put("message", "Data " + tableName + " has been successfully inserted.");
+                    data.put(jsonObject);
+                } else {
+                    jsonObject.put("status_code", 400);
+                    jsonObject.put("message", "Invalid data. Please insert a " + tableName + " valid data.");
+                    data.put(jsonObject);
+                }
+            } else if (tableName.equals("orders")) {
+                Orders orders = new Orders();
+                if (orders.parseOrdersJSON(json) != 1) {
+                    orders.insertOrder();
+                    jsonObject.put("status_code", 200);
+                    jsonObject.put("message", "Data " + tableName + " has been successfully inserted.");
+                    data.put(jsonObject);
+                } else {
+                    jsonObject.put("status_code", 400);
+                    jsonObject.put("message", "Invalid data. Please insert a " + tableName + " valid data.");
+                    data.put(jsonObject);
+                }
+            } else if (tableName.equals("order_details")) {
+                OrderDetails orderDetails = new OrderDetails();
+                if (orderDetails.parseOrderDetailsJSON(json) != 1) {
+                    orderDetails.insertOrderDetails();
+                    jsonObject.put("status_code", "200");
+                    jsonObject.put("message", "Data " + tableName + " has been successfully inserted.");
+                    data.put(jsonObject);
+                } else {
+                    jsonObject.put("status_code", 400);
+                    jsonObject.put("message", "Invalid data. Please insert a " + tableName + " valid data.");
+                    data.put(jsonObject);
+                }
+            } else if (tableName.equals("reviews")) {
+                Reviews reviews = new Reviews();
+                if (reviews.parseReviewsJSON(json) != 1) {
+                    reviews.insertReview();
+                    jsonObject.put("status_code", 200);
+                    jsonObject.put("message", "Data " + tableName + " has been successfully inserted.");
+                    data.put(jsonObject);
+                } else {
+                    jsonObject.put("status_code", 400);
+                    jsonObject.put("message", "Invalid data. Please insert a " + tableName + " valid data.");
+                    data.put(jsonObject);
+                }
+            }
+
+            statusCode = jsonObject.getInt("status_code");
+            String response = data.toString(2);
+            exchange.sendResponseHeaders(statusCode, response.getBytes().length);
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } finally {
+            outputStream.close();
         }
-
-        InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
-        BufferedReader br = new BufferedReader(isr);
-        int i;
-        StringBuilder buf = new StringBuilder();
-
-        while ((i = br.read()) != -1) {
-            buf.append((char) i);
-        }
-
-        br.close();
-        isr.close();
-        String json = buf.toString();
-
-        if (tableName.equals("users")) {
-            Users user = new Users();
-            if (user.parseUserJSON(json) != 1) {
-                user.insertUser();
-                response = "Data has been successfully inserted.";
-                statusCode = 200;
-            } else {
-                response = "Invalid data.";
-                statusCode = 400;
-            }
-        } else if (tableName.equals("addresses")) {
-            Addresses addresses = new Addresses();
-            if (addresses.parseAddressesJSON(json) != 1) {
-                addresses.insertAddress();
-                response = "Data has been successfully inserted.";
-                statusCode = 200;
-            } else {
-                response = "Invalid data.";
-                statusCode = 400;
-            }
-        } else if (tableName.equals("products")) {
-            Products products = new Products();
-            if (products.parseProductsJSON(json) != 1) {
-                products.insertProduct();
-                response = "Data has been successfully inserted.";
-                statusCode = 200;
-            } else {
-                response = "Invalid data.";
-                statusCode = 400;
-            }
-        } else if (tableName.equals("orders")) {
-            Orders orders = new Orders();
-            if (orders.parseOrdersJSON(json) != 1) {
-                orders.insertOrder();
-                response = "Data has been successfully inserted.";
-                statusCode = 200;
-            } else {
-                response = "Invalid data.";
-                statusCode = 400;
-            }
-        } else if (tableName.equals("order_details")) {
-            OrderDetails orderDetails = new OrderDetails();
-            if (orderDetails.parseOrderDetailsJSON(json) != 1) {
-                orderDetails.insertOrderDetails();
-                response = "Data has been successfully inserted.";
-                statusCode = 200;
-            } else {
-                response = "Invalid data.";
-                statusCode = 400;
-            }
-        } else if (tableName.equals("reviews")) {
-            Reviews reviews = new Reviews();
-            if (reviews.parseReviewsJSON(json) != 1) {
-                reviews.insertReview();
-                response = "Data has been successfully inserted.";
-                statusCode = 200;
-            } else {
-                response = "Invalid data.";
-                statusCode = 400;
-            }
-        }
-
-        exchange.sendResponseHeaders(statusCode, response.getBytes().length);
-        outputStream.write(response.getBytes());
-        outputStream.flush();
-        outputStream.close();
     }
 }
